108cfa3c019fe98f9ca0e32da51b16d80797e73a
Dmitriy <dimak24.05@mail.ru>
2019-12-10 00:08:34 +0300

    more

--- functor/main.cpp
+++ functor/main.cpp
@@ -10,7 +10,7 @@ class Visitor;
 
 class Acceptor {
 public:
-    virtual void accept(Visitor&) = 0;
+    virtual void accept(Visitor*) = 0;
 };
 
 
@@ -21,52 +21,52 @@ class ThirdAcceptor;
 
 class Visitor {
 public:
-    virtual void visit(FirstAcceptor&) = 0;
-    virtual void visit(SecondAcceptor&) = 0;
-    virtual void visit(ThirdAcceptor&) = 0;
+    virtual void visit(FirstAcceptor*) = 0;
+    virtual void visit(SecondAcceptor*) = 0;
+    virtual void visit(ThirdAcceptor*) = 0;
 };
 
 
 class FirstAcceptor : public Acceptor {
 public:
-    void accept(Visitor& visitor) override
+    void accept(Visitor* visitor) override
     {
-        visitor.visit(*this);
+        visitor->visit(this);
     }
 };
 
 
 class SecondAcceptor : public Acceptor {
 public:
-    void accept(Visitor& visitor) override
+    void accept(Visitor* visitor) override
     {
-        visitor.visit(*this);
+        visitor->visit(this);
     }
 };
 
 
 class ThirdAcceptor : public Acceptor {
 public:
-    void accept(Visitor& visitor) override
+    void accept(Visitor* visitor) override
     {
-        visitor.visit(*this);
+        visitor->visit(this);
     }
 };
 
 
 class ConcreteVisitor : public Visitor {
 public:
-    void visit(FirstAcceptor&) override
+    void visit(FirstAcceptor*) override
     {
         puts("I'm visiting FirstAcceptor");
     }
 
-    void visit(SecondAcceptor&) override
+    void visit(SecondAcceptor*) override
     {
         puts("I'm visiting SecondAcceptor");
     }
 
-    void visit(ThirdAcceptor&) override
+    void visit(ThirdAcceptor*) override
     {
         puts("I'm visiting ThirdAcceptor");
     }
@@ -80,7 +80,7 @@ private:
     Visitor visitor_;
 
 public:
-    Functor(Acceptor acceptor, Visitor visitor)
+    explicit Functor(Acceptor acceptor, Visitor visitor)
         : acceptor_(acceptor), visitor_(visitor)
     {}
 
@@ -88,30 +88,119 @@ public:
     void operator()(Args&&... args)
     {
         ((void)args, ...);
-        acceptor_.accept(visitor_);
+        acceptor_.accept(&visitor_);
     }
 };
+
+template <bool Condition, typename OnTrue, typename OnFalse>
+struct conditional;
+
+template <typename OnTrue, typename OnFalse>
+struct conditional<true, OnTrue, OnFalse> {
+    using type = OnTrue;
+};
+
+template <typename OnTrue, typename OnFalse>
+struct conditional<false, OnTrue, OnFalse> {
+    using type = OnFalse;
+};
+
+template <bool Condition, typename OnTrue, typename OnFalse>
+using conditional_t = typename conditional<Condition, OnTrue, OnFalse>::type;
+
+
+template <typename Base>
+std::true_type is_base_of_test_(Base&&);
+
+template <typename>
+std::false_type is_base_of_test_(...);
+
+template <typename Base, typename T>
+struct is_base_of : decltype(is_base_of_test_<Base>(std::declval<T>())) {};
+
+template <typename Base, typename T>
+inline constexpr bool is_base_of_v = is_base_of<Base, T>::value;
+
+
+template <typename TL, typename Base>
+struct Split {
+    using accept = typelist::EmptyList;
+    using reject = typelist::EmptyList;
+};
+
+template <typename H, typename... T, typename Base>
+class Split<typelist::TypeList<H, T...>, Base> {
+private:
+    using _accept_prev = typename Split<typelist::TypeList<T...>, Base>::accept;
+    using _reject_prev = typename Split<typelist::TypeList<T...>, Base>::reject;
+
+public:
+    using accept = conditional_t<
+        is_base_of_v<Base, H>,
+        typelist::Append<_accept_prev, H>,
+        _accept_prev
+    >;
+
+    using reject = conditional_t<
+        is_base_of_v<Base, H>,
+        _reject_prev,
+        typelist::Append<_reject_prev, H>
+    >;
+};
+
+template <typename T>
+std::true_type is_acceptor_(decltype(&T::accept));
+
+template <typename>
+std::false_type is_acceptor_(...);
+
+template <typename T>
+struct is_acceptor : decltype(is_acceptor_<T>(nullptr)) {};
+
+template <typename T, typename... Acceptors, typename = std::enable_if_t<  &T::visit  >>
+std::true_type can_visit_()
+
+// template <typename>
+// std::false_type is_visitor_(...);
+
+template <typename T>
+struct is_visitor : decltype(is_visitor_<T>(nullptr)) {};
 
 
 template <typename TL>
 class VisitFactory;
 
-template <typename Acceptor, typename Visitor, typename Functor>
-class VisitFactory<typelist::TypeList<Acceptor, Visitor, Functor>> {
+template<typename... T>
+class VisitFactory<typelist::TypeList<T...>> {
+private:
+
 public:
     template <typename... Args>
     decltype(auto) go(Acceptor* acceptor, Visitor* visitor, Args&&... args) const
     {
-        Functor f(*acceptor, *visitor);
+        // if (d_c && d_c)
+        Functor<Acceptor, Visitor> f(*acceptor, *visitor);
         f(std::forward<Args>(args)...);
     }
 };
 
+template <typename>
+struct ShowType {
+    ShowType() = delete;
+};
+
+struct A {
+    explicit A() = delete;
+};
+
+struct B : A {
+    B() = delete;
+};
 
 int main()
 {
-    VisitFactory<typelist::TypeList<SecondAcceptor, ConcreteVisitor, Functor<SecondAcceptor, ConcreteVisitor>>> f;
-    ConcreteVisitor v;
-    SecondAcceptor a;
-    f.go(&a, &v);
+    bool b = is_visitor<ConcreteVisitor>::value;
+    printf("%d\n", b);
+    // printf("%zu %zu\n", sizeof(_One), sizeof(_Two));
+    // ShowType<typename Split<typelist::TypeList<Acceptor, ThirdAcceptor, Visitor, ConcreteVisitor, FirstAcceptor>, Acceptor>::reject> _;
 }
